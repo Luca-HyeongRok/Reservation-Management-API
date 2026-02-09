c8ad1526e3e924d6843d9908280756c23206320e
JEON HYEONGROK(LUCA) <hr.jeon28@gmail.com>
2026-02-09 23:43:44 +0900

    feat: initial commit – Reservation Management API

---
+++ Study/Reservation-Management-API/src/main/java/com/reservation/management/api/ReservationManagementApiApplication.java
@@ -0,0 +1,12 @@
+package com.reservation.management.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationManagementApiApplication.class, args);
+    }
+}
